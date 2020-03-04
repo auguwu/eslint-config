0604555d5be4e0f42783530c78b0a9da13de91f4
Noel <augu@voided.pw>
2020-03-03 21:27:01 -0700

    Upgrade TS ESLint, added more ESLint rules

--- react.js
+++ react.js
@@ -7,6 +7,11 @@ module.exports = {
   extends: join(__dirname, 'index.js'),
   plugins: ['react'],
   rules: {
-    'react/require-render-return': 'error'
+    'react/require-render-return': 'error',
+    'react/prefer-es6-class': 'error'
+  },
+  settings: {
+    // Always detect the React version
+    react: 'detect' 
   }
 };
