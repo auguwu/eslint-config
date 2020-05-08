536e110b5ca85a5dc939f866af5d20e42e423db8
Noel <augu@voided.pw>
2020-05-08 16:05:38 -0700

    Upgrade ESLint, update rules Added:     - @typescript-eslint/dot-notation     - @typescript-eslint/camelcase     - @typescript-eslint/ban-types     - no-unexpected-multiline     - no-unsafe-negation     - no-unsage-finally     - no-sparse-arrays     - no-unreachable     - camelcase
    Removed:
       - no-invalid-this

--- ts.js
+++ ts.js
@@ -16,7 +16,27 @@ module.exports = {
     '@typescript-eslint/class-name-casing': 'warn',
     '@typescript-eslint/no-extra-semi': 'error',
     '@typescript-eslint/no-namespace': ['error', { 'allowDeclarations': true }],
+    '@typescript-eslint/dot-notation': ['error', { allowPrivateClassPropertyAccess: true }],
     '@typescript-eslint/array-type': 'error',
-    '@typescript-eslint/brace-style': ['error', '1tbs']
+    '@typescript-eslint/brace-style': ['error', '1tbs'],
+    '@typescript-eslint/camelcase': ['error', { genericTypes: 'always' }],
+    '@typescript-eslint/ban-types': ['warn', {
+      types: {
+        "{}": {
+          "message": "Use an object instead",
+          "fixWith": "object"
+        },
+        "Number": {
+          "message": "Use `number` instead",
+          "fixWith": "number"
+        },
+        "String": {
+          "message": "Use `string` instead",
+          "fixWith": "string"
+        }
+      }
+    }],
+    'dot-notation': 'off',
+    camelcase: 'off' // Use TS ESLint's instead of the normal one
   }
 };
