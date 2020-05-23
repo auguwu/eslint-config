0d875035ca8ce72c4c9ae38d25cfc35024432dbe
Noel <augu@voided.pw>
2020-05-22 19:11:56 -0700

    Remove deprecated rule

--- ts.js
+++ ts.js
@@ -13,7 +13,6 @@ module.exports = {
     '@typescript-eslint/no-array-constructor': 'error',
     '@typescript-eslint/no-empty-interface': 'error',
     '@typescript-eslint/no-empty-function': 'error',
-    '@typescript-eslint/class-name-casing': 'warn',
     '@typescript-eslint/no-extra-semi': 'error',
     '@typescript-eslint/no-namespace': ['error', { 'allowDeclarations': true }],
     '@typescript-eslint/array-type': 'error',
