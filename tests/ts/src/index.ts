62d81f223eb930252b56d0ef8f39e2620e52407f
Noel <august@augu.dev>
2021-01-30 10:31:31 -0700

    Update LICENSE year (30 days late, I know!) and remove trailing spaces

--- tests/ts/src/index.ts
+++ tests/ts/src/index.ts
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2019-2020 August
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 // This file shows of what the config is like (w/o issues)
 
 // rule: @typescript-eslint/adjacent-overload-signatures
@@ -40,8 +41,8 @@ interface ABCD {
 const array: string[] = [];
 
 // rule: @typescript-eslint/no-empty-interface
-interface A { 
-  x: string; 
+interface A {
+  x: string;
 }
 
 // rule: @typescript-eslint/no-empty-function
