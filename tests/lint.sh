3b952dc731e2125397b1b3ed0c09b4b4659dba4b
Noel <cutie@floofy.dev>
2021-04-12 16:38:35 -0700

    Fix license in tests/lint.sh, hopefully fix workflow, went from peer -> optional deps

--- tests/lint.sh
+++ tests/lint.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020 August
+# Copyright (c) 2019-2021 August
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,4 +19,4 @@
 # SOFTWARE.
 
 echo 'Now linting JavaScript files...'
-eslint ./tests/js/src --ext .js --fix
+eslint ./tests/src --ext .js --fix
